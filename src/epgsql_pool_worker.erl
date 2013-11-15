89c0988f9a6efc767acc5d9f23da996a94d5f59e
Kurt Williams <kurt.r.williams@gmail.com>
2013-11-15 13:03:07 -0600

    Disconnect after each query

--- src/epgsql_pool_worker.erl
+++ src/epgsql_pool_worker.erl
@@ -7,8 +7,6 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
          code_change/3]).
 
--record(state, {conn, connect_args}).
-
 
 % public api
 
@@ -31,40 +29,34 @@ init(Args) ->
     Username = proplists:get_value(username, Args),
     Password = proplists:get_value(password, Args),
     Timeout = proplists:get_value(timeout, Args),
-    Opts = [{database, Database}, {timeout, Timeout}],
-    {ok, #state{connect_args={Hostname, Username, Password, Opts}}}.
+    Port = proplists:get_value(port, Args),
+    Opts = [{database, Database}, {timeout, Timeout}, {port, Port}],
+    {ok, {Hostname, Username, Password, Opts}}.
 
 
 %% handle call
 
 handle_call({squery, Stmt}, _From, State) ->
-    {Conn, Result} = case connect(State) of
-        {ok, Conn1} ->
-            {Conn1, do_squery(Conn1, Stmt)};
+    Result = case connect(State) of
+        {ok, Conn} ->
+            Resp = do_squery(Conn, Stmt),
+            disconnect(Conn),
+            Resp;
         {error, _Error} ->
-            {undefined, {error, unavailable}}
+            {error, unavailable}
     end,
-    {reply, Result, State#state{conn=Conn}};
+    {reply, Result, State};
 
 handle_call({equery, Stmt, Params}, _From, State) ->
-    {Conn, Result} = case connect(State) of
-        {ok, Conn1} ->
-            {Conn1, do_equery(Conn1, Stmt, Params)};
-        {error, _Error} ->
-            {undefined, {error, unavailable}}
-    end,
-    {reply, Result, State#state{conn=Conn}};
-
-handle_call(connect, _From, #state{conn=undefined}=State) ->
-    case connect(State) of
+    Result = case connect(State) of
         {ok, Conn} ->
-            {reply, ok, State#state{conn=Conn}};
+            Resp = do_equery(Conn, Stmt, Params),
+            disconnect(Conn),
+            Resp;
         {error, _Error} ->
-            {reply, ok, State#state{conn=undefined}}
-    end;
-
-handle_call(connect, _From, State) ->
-    {reply, connected, State};
+            {error, unavailable}
+    end,
+    {reply, Result, State};
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
@@ -78,9 +70,6 @@ handle_cast(_Msg, State) ->
 
 %% handle_info
 
-handle_info({'EXIT',Conn,sock_closed}, #state{conn=Conn}=State) ->
-    {noreply, State#state{conn=undefined}};
-
 handle_info(Info, State) ->
     lager:debug("Info: ~p~nState: ~p~n", [Info, State]),
     {noreply, State}.
@@ -88,11 +77,7 @@ handle_info(Info, State) ->
 
 %% terminate
 
-terminate(_Reason, #state{conn=undefined}) ->
-    ok;
-
-terminate(_Reason, #state{conn=Conn}) ->
-    ok = pgsql:close(Conn),
+terminate(_Reason, _State) ->
     ok.
 
 
@@ -104,14 +89,7 @@ code_change(_OldVsn, State, _Extra) ->
 
 % private functions
 
-connect(#state{conn=Conn}=State) when is_pid(Conn) ->
-    case is_process_alive(Conn) of
-        true  -> {ok, Conn};
-        false -> connect(State#state{conn=undefined})
-    end;
-
-
-connect(#state{connect_args={Hostname, Username, Password, Opts}}) ->
+connect({Hostname, Username, Password, Opts}) ->
     case pgsql:connect(Hostname, Username, Password, Opts) of
         {ok, Conn} ->
             {ok, Conn};
@@ -120,6 +98,8 @@ connect(#state{connect_args={Hostname, Username, Password, Opts}}) ->
             {error, Error}
     end.
 
+disconnect(Conn) ->
+    pgsql:close(Conn).
 
 do_squery(Conn, Stmt) ->
     try_query(fun() -> pgsql:squery(Conn, Stmt) end).
